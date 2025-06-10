391b71377cafd5a4d963f5a2ee0a8f7f4e171a46
ffleita <facundoezequielfleitas96@gmail.com>
2025-06-10 14:25:23 -0300

    Modifico layout de paginas privadas para regresar al login si no se autentico el usuario

--- src/layouts/PrivatePagesLayout.jsx
+++ src/layouts/PrivatePagesLayout.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useEffect } from 'react';
 import { NavigationBar } from '../components/NavigationBar'
+import { useNavigate } from 'react-router-dom'
 
 export const PrivatePagesLayout = ({children}) => {
+
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+      if (!localStorage.getItem('token')) {
+        navigate("/login", {replace: true});
+      }
+    }, [localStorage.getItem('token')])
+
     return (
         <>
             <NavigationBar />
