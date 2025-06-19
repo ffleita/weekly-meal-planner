a608d7197ba5c1f73ccf134fc589dd26b61718ec
Facundo Fleitas <ff@qubikcommerce.com>
2025-06-19 18:32:30 -0300

    Integro detalle de receta con back

--- src/pages/RecetasDetailPage.jsx
+++ src/pages/RecetasDetailPage.jsx
@@ -1,44 +1,19 @@
 import { PrivatePagesLayout } from '../layouts/PrivatePagesLayout'
 import { useNavigate, useParams } from 'react-router-dom'
-import { useState } from 'react';
-
-const recetaDummy = {
-    nombre: 'Sopa de garrote',
-    ingredientesReceta: [
-        {
-            ingrediente: 1,
-            cantidad: 2,
-            medida: 'unidad'
-        }
-    ],
-    pasos: 'Nisi esse excepteur fugiat eu.'
-}
-
-const ingredientes = [
-    {
-        id: 1,
-        nombre: 'zanahoria'
-    },
-    {
-        id: 2,
-        nombre: 'brocoli'
-    },
-    {
-        id: 3,
-        nombre: 'leche'
-    },
-    {
-        id: 4,
-        nombre: 'milanesa'
-    }
-]
+import { useEffect, useState } from 'react'
+import api from '../api/axiosInstance'
 
 export const RecetasDetailPage = () => {
 
-    // Extraigo el idReceta de los parámetros de la URL
     const { idReceta } = useParams();
 
-    const [receta] = useState(recetaDummy);
+    const [receta, setReceta] = useState()
+
+    const [ingredientes, setIngredientes] = useState([])
+
+    const [medidas, setMedidas] = useState([])
+
+    const [error, setError] = useState("")
 
     const navigate = useNavigate();
 
@@ -47,38 +22,65 @@ export const RecetasDetailPage = () => {
         navigate(-1);
     }
 
+    const loadRecetaDetailPageData = async (id) => {
+        try {
+            const recetaDetailResponse = await api.get(`/recetas/${id}`);
+            setReceta(recetaDetailResponse.data);
+            const ingredientesResponse = await api.get('/ingredientes');
+            setIngredientes(ingredientesResponse.data.ingredientes);
+            const medidasResponse = await api.get('/medidas');
+            setMedidas(medidasResponse.data);
+        } catch (e) {
+            setError(e.message)
+        }
+    }
+
+    useEffect(() => {
+        loadRecetaDetailPageData(idReceta);
+    }, [])
+
     return (
         <PrivatePagesLayout>
-            <br />
-            <h4><u>Receta</u></h4>
-            <h1>{receta.nombre}</h1>
-            <hr />
-
-            <div className='row mb-3'>
-                <div className='col'>
-                    <h6>Ingredientes</h6>
-                    <ul className="list-group">
-                        {receta.ingredientesReceta.map(ingredienteReceta => (
-                            <li id={ingredienteReceta.ingrediente} className="list-group-item">{ingredientes.find(e => e.id == ingredienteReceta.ingrediente).nombre} - {ingredienteReceta.cantidad} {ingredienteReceta.medida}</li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-            <div className='row mb-3'>
-                <div className='col'>
-                    <h6>Pasos</h6>
-                    <div className="">
-                        <textarea className="form-control" name="pasos" id="pasos" rows="5" disabled>{receta.pasos}</textarea>
+            {!error && receta && (
+                <>
+                    <br />
+                    <h4><u>Receta</u></h4>
+                    <h1>{receta.nombre}</h1>
+                    <hr />
+                    <div className='row mb-3'>
+                        <div className='col'>
+                            <h6>Ingredientes</h6>
+                            <ul className="list-group">
+                                {
+                                    receta.ingredientesReceta.map(ingredienteReceta => {
+                                        const ingrediente = ingredientes.find(e => e.id === ingredienteReceta.ingrediente);
+                                        const medida = medidas.find(m => m.id === ingredienteReceta.medida);
+                                        return (
+                                            <li key={ingredienteReceta.ingrediente} className="list-group-item">
+                                                {ingrediente ? ingrediente.nombre : ''} - {ingredienteReceta.cantidad} {medida ? medida.nombre : ''}
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className='row d-flex justify-content-end'>
-                <div className='col-sm-12 col-md-2'>
-                    <button type='button' className='btn btn-secondary w-100' onClick={handleGoBack}>Regresar</button>
-                </div>
-            </div>
-
-
+                    <div className='row mb-3'>
+                        <div className='col'>
+                            <h6>Pasos</h6>
+                            <div className="">
+                                <textarea className="form-control" name="pasos" id="pasos" rows="5" disabled>{receta.pasos}</textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='row d-flex justify-content-end'>
+                        <div className='col-sm-12 col-md-2'>
+                            <button type='button' className='btn btn-secondary w-100' onClick={handleGoBack}>Regresar</button>
+                        </div>
+                    </div>
+                </>
+            )}
+            {error && (<div className='alert alert-danger'>{error}</div>)}
         </PrivatePagesLayout>
     )
 }
